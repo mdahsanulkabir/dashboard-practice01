1ea796b1027e9d703ce454fd57e34822cd93c678
ahsan <makabir98@yahoo.com>
2023-01-14 18:25:09 +0600

    sidebar prepared

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { Box, Typography, useTheme } from "@mui/material"
+import { fontWeight } from "@mui/system";
+import { tokens } from "../theme";
+
+const Header = ({ title, subTitle }) => {
+
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode)
+    return (
+        <Box mb="30px">
+            <Typography 
+                variant="h2" 
+                color={colors.grey[100]}
+                fontWeight="bold" 
+                sx={{mb : "5px"}}
+            >{title}</Typography>
+            <Typography variant="h5" color={colors.greenAccent[400]}>{subTitle}</Typography>
+        </Box>
+    )
+}
+
+export default Header;
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,6 +1,12 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header"
 const Dashboard = () => {
     return (
-        <div> Dashboard </div>
+        <Box m="20px"> 
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="DASHBOARD" subTitle="Welcome to your dashboard"></Header>   
+            </Box>
+        </Box>
     )
 }
 
